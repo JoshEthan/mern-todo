274dcfda1854d0cb69743624b7eb6298330ae01c
JoshEthan <joshuaeris@gmail.com>
2023-02-20 09:54:10 -0700

    Ready to deploy

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+mongoose.connect('mongodb+srv://user1:123@cluster0.xdhn07h.mongodb.net/?retryWrites=true&w=majority', {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
